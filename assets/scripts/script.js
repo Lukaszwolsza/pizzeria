4ceabfb1343bb49a7d955f555412b37cf7f1dd26
Łukasz Wolsza <luasply@gmail.com>
2020-02-06 21:01:32 +0100

    add fresh modern design

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,12 @@
 const btnToCreatePizza = document.getElementById('create-pizza');
 const creatingPizzaDiv = document.getElementById('add-modal');
-
+const darkModeActive = document.getElementById('add-dark-mode');
+const hiddingWelcomeText = document.getElementById('hide-welcome-text');
 
 const showPizza = () => {
     creatingPizzaDiv.classList.toggle('visible');
+    darkModeActive.classList.toggle('visible');
+    hiddingWelcomeText.classList.toggle('invisible');
 };
 
 btnToCreatePizza.addEventListener('click', showPizza);
