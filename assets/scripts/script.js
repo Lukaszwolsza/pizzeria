d44aac3c601e23ad576bd4d214cc06b20e6f0923
Łukasz Wolsza <luasply@gmail.com>
2020-02-08 15:23:13 +0100

    add some features like displaying ordeR

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,44 @@
-const btnToCreatePizza = document.getElementById('create-pizza');
 const creatingPizzaDiv = document.getElementById('add-modal');
 const darkModeActive = document.getElementById('add-dark-mode');
 const hiddingWelcomeText = document.getElementById('hide-welcome-text');
-const pizzaSize = document.getElementById('inputedSize');
-const pizzaName = document.getElementById('pizzaName').value;
 const addNewIngredient = document.getElementById('newIngredient');
 const table = document.querySelector('tbody');
 const ingredientsDiv = document.querySelector('.ingredients-box');
 const removeIngredient = document.getElementById('removeIngredient');
-const btnAddNewPizza = document.getElementById('btnToAddPizza');
 const inputedIngredientsDynamic = document.querySelector('.inputed-dynamic-ingredients');
 const pizzaMenu = document.getElementById('pizzasMenu');
+const orderInfoBox = document.getElementById('orderStatus');
+
+//Buttons
 const btnToContact = document.getElementById('contactBox');
+const btnAddNewPizza = document.getElementById('btnToAddPizza');
+const btnToCreatePizza = document.getElementById('create-pizza');
+const btnToMakeNewOrder = document.getElementById('btnToAddPizza');
+
+
+// RESULT DATA DISPLAYING
+const streetResult = document.getElementById('streetResult');
+const flatResult = document.getElementById('flatResult');
+const postcodeResult = document.getElementById('postcodeResult');
+const cityResult = document.getElementById('cityResult');
+const pizzaNameResult = document.getElementById('pizzaNameResult');
+const pizzaSizeResult = document.getElementById('pizzaSizeResult');
+const pizzaIngredients = document.getElementById('ingredientsResult');
+
+// const addressInfos = {
+//     street  : streetAddress,
+//     number  : numberAddress,
+//     post    : postCode,
+//     city    : cityAddress
+// };
+
 
 const showPizza = () => {
     creatingPizzaDiv.classList.toggle('visible');
     darkModeActive.classList.toggle('visible');
     hiddingWelcomeText.classList.toggle('invisible');
     pizzaMenu.classList.toggle('visible');
+    
 };
 
 const addNewPizza = () => {
@@ -40,11 +61,41 @@ const rmvNewIngr = () => {
     if(inputedIngredientsDynamic.children.length > 1){
         inputedIngredientsDynamic.lastChild.remove();
     } else {
-        alert ('cannot remove last element !');
+        alert ('Cannot remove last element !');
     }
+};
+
+function checkPizzaSize(){
+    if(pizzaSize === 30 || pizzaSize === 40 || pizzaSize === 50){
+        pizzaSizeResult.textContent = pizzaSize;
+    } else {
+        alert('You inputed incorrect pizza size');
+    }
+}
+
+const showFinalInfoAboutOrder = () => {
+    orderInfoBox.classList.toggle('visible');
+    creatingPizzaDiv.classList.toggle('visible');
+
+    const streetAddress = document.getElementById('streetInfo').value;
+    const numberAddress = document.getElementById('flatNumberInfo').value;
+    const postCode = document.getElementById('postCodeInfo').value;
+    const cityAddress = document.getElementById('cityInfo').value;
+    const pizzaSize = document.getElementById('inputedSize').value;
+    const pizzaName = document.getElementById('pizzaName').value;
+
+    pizzaNameResult.textContent = pizzaName;
+    checkPizzaSize();
+    streetResult.textContent = streetAddress;
+    flatResult.textContent = numberAddress;
+    postcodeResult.textContent = postCode;
+    cityResult.textContent = cityAddress;
+    
 };
 
 btnToCreatePizza.addEventListener('click', showPizza);
 addNewIngredient.addEventListener('click', addNewIngr);
 removeIngredient.addEventListener('click', rmvNewIngr);
 btnAddNewPizza.addEventListener('click', addNewPizza);
+btnToMakeNewOrder.addEventListener('click', showFinalInfoAboutOrder);
+
